5ea9953b63c10089bd00f11ea3ede560fc2d6ff1
brianalban07 <brianalban07@gmail.com>
2026-05-20 12:31:18 +0800

    Update fileWorker.js

--- fileWorker.js
+++ fileWorker.js
@@ -7,22 +7,21 @@ self.onmessage = function(e) {
 
   buffers.forEach(buffer => {
 
-    const wb = XLSX.read(buffer,{type:'array'});
+    const wb = XLSX.read(buffer, {type:'array'});
     const sheet = wb.Sheets[wb.SheetNames[0]];
     const json = XLSX.utils.sheet_to_json(sheet);
 
-    let dur = {};
-    let cnt = {};
+    let dur={}, cnt={};
 
     json.forEach(r=>{
-      const s = r.Stage;
-      const st = new Date(r.StartTime);
-      const et = new Date(r.EndTime);
+      const s=r.Stage;
+      const st=new Date(r.StartTime);
+      const et=new Date(r.EndTime);
 
-      if (s && !isNaN(st) && !isNaN(et)){
-        const d = (et-st)/1000;
+      if(s && !isNaN(st) && !isNaN(et)){
+        const d=(et-st)/1000;
 
-        if (!dur[s]) { dur[s]=0; cnt[s]=0; }
+        if(!dur[s]){dur[s]=0;cnt[s]=0;}
 
         dur[s]+=d;
         cnt[s]++;
@@ -34,4 +33,3 @@ self.onmessage = function(e) {
 
   self.postMessage({results});
 };
-``
